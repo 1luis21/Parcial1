9c20e7e2a301887a6fce91b083e437147782b52f
1luis21 <60717314+1luis21@users.noreply.github.com>
2020-02-10 16:35:10 -0600

    Add files via upload

---
+++ Calificacion.js
@@ -0,0 +1,16 @@
+var num = Math.floor(Math.random()*10)+1;
+
+console.log(num); //imprimimos variable generada aleatoriamente para corroborar
+
+if(num==10)
+    console.log("Calificacion es: A");
+else if(num==9)
+    console.log("Calificacion es: B");
+else if(num==8)
+    console.log("Calificacion es: C");
+else if(num==7)
+    console.log("Calificacion es: D");
+else if(num==6)
+    console.log("Calificacion es: E");
+else
+    console.log("Calificacion es: F");
